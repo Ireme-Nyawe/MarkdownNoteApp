7d6362b0f44686aca71a35aa789a47bfbd3182ac
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-18 09:17:34 +0200

    add functionality in app

---
+++ src/data.jsx
@@ -0,0 +1,29 @@
+export default data = [
+  {
+    id: 1,
+    body: `### this just in!
+    
+    Markdoun is *awesome* way to write **very basic html** by typing manually
+    
+    it can be used for : 
+    1. README files
+    2. Slack and discord messages
+    3.**so much more!**
+    
+    ---
+    
+    here is division`,
+},
+{
+    id:2,body:`a`
+},
+{
+    id:3,body:`b`
+},{
+    id:4,body:`c`
+},{
+    id:5,body:`d`
+},{
+    id:6,body:`e`
+},
+];
